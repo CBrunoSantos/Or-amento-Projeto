d55220f13226320ba67636f2fe2b06c670a59e5b
CBrunoSantos <brunosantos@edu.unifor.br>
2021-04-06 12:26:24 -0300

    criando o banco de dados local storage

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+class Despesa{
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
+
+class Bd{
+    constructor(){
+        let id = localStorage.getItem('id');
+
+        if(id === null){
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximmoId(){
+        let proximoid = localStorage.getItem('id');
+        return parseInt(proximoid) + 1;
+    }
+    gravar (d){
+        let id = this.getProximmoId();
+        localStorage.setItem(id, JSON.stringify(d));
+        localStorage.setItem('id', id);
+    }
+}
+
+let bd = new Bd;
+
 function cadastrarDespesas(){
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
@@ -6,6 +39,18 @@ function cadastrarDespesas(){
     let descricao = document.getElementById('descricao');
     let valor = document.getElementById('valor');
 
-    console.log(ano.value)
+
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    )
+
+    console.log(despesa);
+    bd.gravar(despesa);
 
 }
+
